5d65bb35a975fc3373c51a8fbab00fc2ffc38713
Ali Abbani <aliabbani08@gmail.com>
2023-07-30 12:02:23 +0100

    trying process.argv

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,13 @@
 const chalk = require('chalk');
-
 const getNotes = require("./node.js");
 
-const msg = getNotes();
-console.log(msg);
+const command = process.argv[2];
 
-const text = "Error!"
-console.log(chalk.red.bold.inverse(text));
+console.log(process.argv)
+
+
+if(command === "add"){
+    console.log("add note")
+} else if(command === "remove") {
+    console.log("remove note")
+}
