5bf4b1a94718823cf6c8a114cc0d00888771aa02
Ali Abbani <aliabbani08@gmail.com>
2023-07-30 12:35:42 +0100

    working and having some fun with the yargs package while creating the notes-app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,41 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require("./node.js");
 
-const command = process.argv[2];
+// create an add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function () {
+        console.log('Adding a new note')
+    }
+})
 
-console.log(process.argv)
+// create a remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new note',
+    handler: function () {
+        console.log('Removing a new note')
+    }
+})
 
+// create a list command
+yargs.command({
+    command: 'list',
+    describe: 'list a new note',
+    handler: function () {
+        console.log('Listing a new note')
+    }
+})
 
-if(command === "add"){
-    console.log("add note")
-} else if(command === "remove") {
-    console.log("remove note")
-}
+// create a read command
+yargs.command({
+    command: 'read',
+    describe: 'read a new note',
+    handler: function () {
+        console.log('Reading a new note')
+    }
+})
+
+console.log(yargs.argv);
